fc7938fcd01f37a9da6ae5d8df4b9389b424f06a
seonim-ryu <seonim.ryu@nhnent.com>
2016-06-14 16:41:48 +0900

    chore : v1.3.1

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-tui.util.defineNamespace('tui.component.Spinbox', require('./src/spinbox'));
-tui.util.defineNamespace('tui.component.TimePicker', require('./src/timepicker'));
-tui.util.defineNamespace('tui.component.DatePicker', require('./src/datepicker'));
+tui.util.defineNamespace('tui.component.Spinbox', require('./src/spinbox'), true);
+tui.util.defineNamespace('tui.component.TimePicker', require('./src/timepicker'), true);
+tui.util.defineNamespace('tui.component.DatePicker', require('./src/datepicker'), true);

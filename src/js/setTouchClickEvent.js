fd60cd8531f3823009a0294cb3e0ecf35edebd40
한정 <hanjung@nhnent.com>
2019-03-26 09:22:10 +0900

    fix: Select events by using mobile(#26) (#27)
    * fix: Select events by using mobile
    
    * chore: add comment to private method
    
    * chore: apply review

--- src/js/setTouchClickEvent.js
+++ src/js/setTouchClickEvent.js
@@ -5,9 +5,16 @@
 
 'use strict';
 
-var snippet = require('tui-code-snippet');
-
 var $ = require('jquery');
+
+/**
+ * Detect mobile browser
+ * @private
+ * @returns {boolean} Whether using Mobile browser
+ */
+function isMobile() {
+    return /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+}
 
 /**
  * For using one - Touch or Mouse Events
@@ -19,25 +26,16 @@ var $ = require('jquery');
  */
 module.exports = function(target, handler, option) {
     var $target = $(target);
-    var eventList = ['touchend', 'click'];
-    var selector, namespace, events;
+    var eventType = isMobile() ? 'touchend' : 'click';
+    var selector, namespace;
 
     option = option || {};
     selector = option.selector || null;
     namespace = option.namespace || '';
 
     if (namespace) {
-        eventList = snippet.map(eventList, function(eventName) {
-            return eventName + '.' + namespace;
-        });
+        eventType = eventType + '.' + namespace;
     }
 
-    events = eventList.join(' ');
-    $target.on(events, selector, function onceHandler(ev) {
-        var newEventName = ev.type + '.' + namespace;
-
-        handler(ev);
-        $target.off(events, selector, onceHandler)
-            .on(newEventName, selector, handler);
-    });
+    $target.on(eventType, selector, handler);
 };
